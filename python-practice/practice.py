3b8b5c4f9fc4a661fc5dbeb5e52d50453c50677f
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-01-26 18:35:36 -0700

    Modify webpage replication 3, add css grid practice

--- python-practice/practice.py
+++ python-practice/practice.py
@@ -1,10 +1,49 @@
-def reverse_str(str):
-    reversed_str = ''
-    for character in str:
-        reversed_str = character + reversed_str
-    return reversed_str
+# def reverse_str(str):
+#     reversed_str = ''
+#     for character in str:
+#         reversed_str = character + reversed_str
+#     return reversed_str
 
 
-original_string = 'Hello, my name is Mark'
-new_string = reverse_str(original_string)
-print(new_string)
+# original_string = 'Hello, my name is Mark'
+# new_string = reverse_str(original_string)
+# print(new_string)
+
+
+# Function 1
+
+def get_largest_int(int_list):
+    largest_int = max(int_list)
+    return largest_int
+
+
+test_list = [0, 12, 4, 5, 0, 200, 3, 0, 1020, 43, 90]
+print("Largest Int: ", get_largest_int(test_list))
+
+
+def get_largest_int_the_hard_way(int_list):
+    largest_int = None
+    for int in int_list:
+        if not largest_int or (largest_int and int > largest_int):
+            largest_int = int
+
+    return largest_int
+
+
+print("Largest Int alt: ", get_largest_int_the_hard_way(test_list))
+
+
+# Function 2
+
+
+def double_vision(str):
+    doubled_str = ''
+    for char in str:
+        doubled_str += char
+        doubled_str += char
+
+    return doubled_str
+
+
+test_str = "Hello, my name is Mark"
+print("Doubled String: ", double_vision(test_str))
