9c36b79da35c432b205436e546a7a31581918beb
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-03-12 14:44:58 -0600

    Adds Class Component Challenges assignment

---
+++ react-assignments/class-component-challenge/src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a test from VS Code</h1>
+        <h1>Testing... testing...</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
